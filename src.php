854296cb085b2eaf1aa37e50552a88c5755026da
Falaleev Maxim <max@studio107.ru>
2016-08-17 09:31:23 +0300

    Refact

--- src.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include __DIR__ . '/src/Mindy/Cache/Cache.php';
-include __DIR__ . '/src/Mindy/Cache/FileCache.php';
-include __DIR__ . '/src/Mindy/Cache/MemCache.php';
-include __DIR__ . '/src/Mindy/Cache/MemCacheServer.php';
-include __DIR__ . '/src/Mindy/Cache/XCache.php';
